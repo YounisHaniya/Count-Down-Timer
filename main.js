f051266175f98b6a555d15b3c298244e6191618d
YounisHaniya <younishaniya96@gmail.com>
2020-10-20 13:07:09 +0300

    push Count Down Timer

---
+++ main.js
@@ -0,0 +1,48 @@
+// var seconds = 200,
+//   CountDiv = document.getElementById("countdown");
+
+// countdown = setInterval(function () {
+//   "use strict";
+//   secondPass();
+// }, 1000);
+
+// function secondPass() {
+//   "use strict";
+//   var minutes = Math.floor(seconds / 60),
+//     remSeconds = seconds % 60;
+//   if (seconds < 10) {
+//     remSeconds = " 0" + remSeconds;
+//   }
+//   CountDiv.innerHTML = minutes + ":" + remSeconds;
+
+//   if (seconds > 0) {
+//     seconds -= 1;
+//   } else {
+//     clearInterval(countdown);
+//     CountDiv.innerHTML = "Done ...";
+//   }
+// }
+var countDate = new Date("1/1/2021").getTime();
+
+function newYear() {
+  var now = new Date().getTime();
+  var gap = countDate - now;
+
+  var second = 1000;
+  var minute = second * 60;
+  var hour = minute * 60;
+  var day = hour * 24;
+
+  var d = Math.floor(gap / day);
+  var h = Math.floor((gap % day) / hour);
+  var m = Math.floor((gap % hour) / minute);
+  var s = Math.floor((gap % minute) / second);
+
+  document.getElementById("day").innerText = d;
+  document.getElementById("hour").innerText = h;
+  document.getElementById("minute").innerText = m;
+  document.getElementById("second").innerText = s;
+}
+setInterval(function () {
+  newYear();
+}, 1000);
